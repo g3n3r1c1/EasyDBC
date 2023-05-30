c71aef0368783b9bba4b9ec5c360b335e6d1aa3d
nour <g3n3r1c2532@gmail.com>
2023-05-30 18:12:56 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import kotlin.script.experimental.jvm.util.classpathFromClass
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "kt.dbc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.20-RC")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20-RC")
+    implementation("org.xerial:sqlite-jdbc:3.40.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+sourceSets.main {
+    java.srcDirs( "src/main/kotlin")
+    kotlin.srcDirs("src/main/kotlin")
+}
+
+
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+    public static void main(String[] args){
+        JDBX.DBC("jdbc:sqlite:./mybase1.db").JableMod("").addRow();
+    }
+}
